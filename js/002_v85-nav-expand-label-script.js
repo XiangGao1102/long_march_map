cd69a62a10b30478bb6b4e821cede1d91bade507
Xiang Gao <gao_x1102@163.com>
2026-09-12 12:29:30 +0800

    delete

--- js/002_v85-nav-expand-label-script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("button, [role='button'], .text-button, span, b").forEach((el) => {
-    if ((el.textContent || "").trim() === "展开导航栏") {
-      el.classList.add("v85-nav-expand-label");
-    }
-  });
-});
